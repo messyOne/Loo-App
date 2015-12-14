d8eb1fe6bb9ba4800d1e253249e4f58945276aca
Daniel Weiss <mail@weiss-daniel.de>
2015-12-14 07:27:05 +0100

    delete global constant

--- config/global.php
+++ config/global.php
@@ -4,6 +4,9 @@ return [
     'default_layout' => 'default',
     'application_name' => 'Loo - Framework',
 
+    'app_path' => 'src/',
+
+    // TODO rename to layout_templates
     'layout_paths' => [
         'default' => 'layout/default',
     ],
--- private/constants.php
+++ private/constants.php
@@ -2,4 +2,3 @@
 
 // TODO check which are needed. get rid of them
 define('ROOT', dirname(__DIR__));
-define('APPLICATION', 'src');
